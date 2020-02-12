b92ec465a3326c5624482fb31c3a465e02a4a920
Martin Beentjes <martin@beentjes.me>
2020-02-12 15:02:30 +0100

    initial code commit

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -l
+
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+echo ::set-output name=current-version:$PACKAGE_VERSION
