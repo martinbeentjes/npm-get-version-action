e6eac59aefeaceb79cc8211ebb63ac645c786ba6
Martin Beentjes <martinbeentjes@users.noreply.github.com>
2020-02-12 15:10:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
-echo ::set-output name=current-version:$PACKAGE_VERSION
+echo ::set-output name=current-version::$PACKAGE_VERSION
