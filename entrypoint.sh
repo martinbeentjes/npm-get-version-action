39955470041ca020b8607552639011d824482cb7
Jan T <jan-tee@users.noreply.github.com>
2020-07-11 11:26:01 +0200

    Support package.json  (#6)
    * Update action.yml
    
    * Update action.yml
    
    * Update entrypoint.sh
    
    * Update README.md
    
    * Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
-
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+PACKAGE_JSON_PATH="${1-.}"
+echo "Reading package.json from ${PACKAGE_JSON_PATH}/package.json"
+PACKAGE_VERSION=$(cat ${PACKAGE_JSON_PATH}/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
 echo ::set-output name=current-version::$PACKAGE_VERSION
