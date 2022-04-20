263f2aeb8aa202a3d270a8e4b23fb0aefc6e50b5
Martin Beentjes <martinbeentjes@users.noreply.github.com>
2022-04-20 12:55:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 PACKAGE_JSON_PATH="${1-.}"
 echo "Reading package.json from ${PACKAGE_JSON_PATH}/package.json"
-PACKAGE_VERSION=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.version')
+PACKAGE_VERSION=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.version' | tr -d '"')
 
 echo ::set-output name=current-version::$PACKAGE_VERSION
