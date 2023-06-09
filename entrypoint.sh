88e6dc94b7ad42d87e9d895ab1ff132d59c7e2cd
Karl Ludwig Weise <ludwig@ludwigweise.de>
2023-06-09 11:53:54 +0200

    feat(version): Added major and minor versions as output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@ PACKAGE_JSON_PATH="${1-.}"
 echo "Reading package.json from ${PACKAGE_JSON_PATH}/package.json"
 PACKAGE_VERSION=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.version' | tr -d '"')
 
-echo "current-version=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
+echo "major=$(cut -d '.' -f 1 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
+echo "minor=$(cut -d '.' -f 1,2 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
+echo "full=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
